e02cb963e51c6366d1eab5c7d208b2c28837ebcb
JeongHeon <kdrlawjdgjs@gmail.com>
2024-12-30 18:18:44 +0900

    feat: login popup 모달 생성

---
+++ app/components/Authentication.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { usePopupState, useToggle } from "../context/Popup";
+
+export default function Authentication() {
+  const isOpen = usePopupState();
+  const toggle = useToggle();
+
+  if (!isOpen) return;
+  return (
+    <div
+      className="w-full absolute top-0 right-0 bottom-0 bg-slate-900/75"
+      onClick={toggle}
+    >
+      <p className="text-white text-lg" onClick={(e) => e.stopPropagation()}>
+        배경
+      </p>
+    </div>
+  );
+}

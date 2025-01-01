0223667184eec9f5f13e5d33ba8e3e5b66e716aa
JeongHeon <kdrlawjdgjs@gmail.com>
2025-01-01 16:25:20 +0900

    feat: 컴포넌트 추가

--- app/components/Authentication.tsx
+++ app/components/Authentication.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import { MouseEvent } from "react";
+import { Card } from "@/components/ui/card";
 import { usePopupState, useToggle } from "../context/Popup";
+import Login from "./Login";
 
 export default function Authentication() {
   const isOpen = usePopupState();
   const toggle = useToggle();
+  const preventPropagation = (e: MouseEvent) => e.stopPropagation();
 
   if (isOpen) {
     return (
@@ -12,9 +16,13 @@ export default function Authentication() {
         className="w-full absolute top-0 right-0 bottom-0 bg-slate-900/75 z-50"
         onClick={toggle}
       >
-        <p className="text-white text-lg" onClick={(e) => e.stopPropagation()}>
-          배경
-        </p>
+        <Card
+          className="absolute top-40 flex flex-col max-w-[350px] mx-auto left-0 right-0"
+          onClick={preventPropagation}
+        >
+          {/* <Signup /> */}
+          <Login />
+        </Card>
       </div>
     );
   }

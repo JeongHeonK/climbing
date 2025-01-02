333d856b4a9d9dc6d43a9a9f0861e2cf3fd25be5
JeongHeon <kdrlawjdgjs@gmail.com>
2025-01-02 23:40:09 +0900

    feat: 중복 확인 수동으로 전환

--- app/api/database.ts
+++ app/api/database.ts
@@ -3,6 +3,21 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function main() {
-  const allUsers = await prisma.user.findMany();
-  return allUsers;
+  const email = "test12@test.com";
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!existingUser) {
+    await prisma.user.create({
+      data: {
+        email,
+        password: "112233!!t",
+      },
+    });
+    return "성공";
+  }
+  return "중복된 아이디 입니다.";
 }

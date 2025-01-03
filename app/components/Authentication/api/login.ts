3add171b169df376ca308d92cabf336c1a5e72b9
JeongHeon <kdrlawjdgjs@gmail.com>
2025-01-03 17:30:17 +0900

    feat: 로그인 로직 구현 완료
    - hash 비교 함수 추가
    - 아이디 및 비밀번호 확인 추가

--- app/components/Authentication/api/login.ts
+++ app/components/Authentication/api/login.ts
@@ -1,14 +1,18 @@
 "use server";
 
 import { checkLoginValidation } from "@/app/util/validation";
+import { userDB } from "@/app/api/database";
+import { ERROR_MESSAGES } from "@/app/constant/constant";
+import bcrypt from "bcryptjs";
+import { FormError } from "../type";
 
 export const login = async (
-  _: { message: string } | undefined,
+  _: FormError | undefined,
   formdata: FormData,
-) => {
+): Promise<FormError> => {
   const input = {
-    email: formdata.get("email"),
-    password: formdata.get("password"),
+    email: formdata.get("email")?.toString(),
+    password: formdata.get("password")?.toString(),
   };
 
   const result = checkLoginValidation(input);
@@ -16,8 +20,26 @@ export const login = async (
   if (!result.success) {
     const [{ message }] = result.error.errors;
 
-    return { result, message };
+    return { state: "error", message };
   }
 
-  return { result: "성공", message: "" };
+  if (input.email !== undefined && input.password !== undefined) {
+    const user = await userDB.findUnique({
+      where: {
+        email: input.email,
+      },
+    });
+
+    if (!user) return { state: "error", message: ERROR_MESSAGES.user };
+
+    if (!(await checkPassword(input.password, user.password))) {
+      return { state: "error", message: ERROR_MESSAGES.pw };
+    }
+  }
+
+  return { state: "success", message: null };
+};
+
+const checkPassword = async (pw: string, hash: string) => {
+  return bcrypt.compare(pw, hash);
 };

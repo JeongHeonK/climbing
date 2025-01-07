126fcb4888c092a0d0729bfc87ad1abfe1b11c13
JeongHeon <kdrlawjdgjs@gmail.com>
2025-01-07 18:00:49 +0900

    feat: detail 페이지 접근 제한 추가

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,18 @@
 import { cookies } from "next/headers";
 import { Auth } from "./app/api/auth";
+import { NextResponse } from "next/server";
 
-export async function middleware() {
+export async function middleware(request: NextResponse) {
   const session = (await cookies()).get("session");
-  if (session) {
+  if (request.url.endsWith("/") && session) {
     await Auth.updateSession();
+  }
+
+  if (request.url.includes("/detail") && !session) {
+    return NextResponse.redirect(new URL("/", request.url));
   }
 }
 
 export const config = {
-  matcher: "/",
+  matcher: ["/", "/detail"],
 };

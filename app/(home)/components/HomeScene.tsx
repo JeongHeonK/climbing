6ab1199c87b66bdc1fcf5dfe629b39ad9e212e44
JeongHeon <kdrlawjdgjs@gmail.com>
2025-01-08 18:47:28 +0900

    fix: 지도 하나만 생성되는 버그 수정
    - Id 값이 같으면 하나의 div로 인식
    - 지도도 한번 생성됨.
    - 각각 다른 Id 부여

--- app/(home)/components/HomeScene.tsx
+++ app/(home)/components/HomeScene.tsx
@@ -1,40 +1,50 @@
+import { Button } from "@/components/ui/button";
 import Gathering from "./Gathering";
 
 export default async function HomeScene() {
   return (
     <main className="w-full">
-      <div className="border px-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-3 py-2 px-12 md:px-3">
+      <div className="px-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-y-8 py-2 px-12 md:px-3 max-w-[1100px] mx-auto">
         <Gathering
           title="양재 클라이밍"
           date={new Date("2024-11-30 03:00")}
           location="더 클라이밍 양재"
+          id="map"
         />
         <Gathering
           title="양재 클라이밍"
           date={new Date("2024-12-07 03:00")}
           location="더 클라이밍 양재"
+          id="map2"
         />
         <Gathering
           title="양재 클라이밍"
           date={new Date("2024-12-14 03:00")}
           location="더 클라이밍 양재"
+          id="map3"
         />
         <Gathering
           title="양재 클라이밍"
           date={new Date("2024-12-21 03:00")}
           location="더 클라이밍 양재"
+          id="map4"
         />
         <Gathering
           title="양재 클라이밍"
           date={new Date("2024-12-28 03:00")}
           location="더 클라이밍 양재"
+          id="map5"
         />
         <Gathering
           title="양재 클라이밍"
           date={new Date("2024-01-04 03:00")}
           location="더 클라이밍 양재"
+          id="map6"
         />
       </div>
+      <Button className="text-2xl font-thin z-50 absolute bottom-1 right-10 rounded-full px-3">
+        +
+      </Button>
     </main>
   );
 }

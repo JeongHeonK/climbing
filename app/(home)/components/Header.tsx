ad76de193c63f02d105b20439adc538e8cf683d6
JeongHeon <kdrlawjdgjs@gmail.com>
2025-01-08 16:48:42 +0900

    feat: logo 추가

--- app/(home)/components/Header.tsx
+++ app/(home)/components/Header.tsx
@@ -7,26 +7,36 @@ import {
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
 import { cookies } from "next/headers";
+import Image from "next/image";
+import Link from "next/link";
 import LogInButton from "./LogInButton";
 
 export default async function Header() {
   const session = (await cookies()).get("session");
+  const isLogin = session !== undefined;
+
   return (
-    <header className="flex w-full border justify-between">
+    <header className="flex w-full border justify-between items-center">
       <NavigationMenu>
-        <NavigationMenuList>
+        <NavigationMenuList className="pl-2 py-1">
           <NavigationMenuItem>
-            <NavigationMenuLink href="/123">logo</NavigationMenuLink>
+            <NavigationMenuLink href="/">
+              <Image src="/logo.png" width={50} height={50} alt="logo" />
+            </NavigationMenuLink>
           </NavigationMenuItem>
           <NavigationMenuItem>
             <NavigationMenuTrigger>Menu</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <div className="p-4 w-[200px]">will be updated soon</div>
+            <NavigationMenuContent className="w-[120px] p-3 ">
+              <Link href="/myPage" className="hover:font-bold transition-all">
+                My Climbing
+              </Link>
             </NavigationMenuContent>
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
-      <LogInButton isLogin={session !== undefined} />
+      <LogInButton isLogin={isLogin}>
+        {isLogin ? "Log In" : "Log Out"}
+      </LogInButton>
     </header>
   );
 }

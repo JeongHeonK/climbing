64d33b9dcfc03b5028e038a7e61688286fc30f77
JeongHeon <kdrlawjdgjs@gmail.com>
2025-01-08 18:46:03 +0900

    feat: 지도 구현

--- app/(home)/components/Gathering.tsx
+++ app/(home)/components/Gathering.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Card,
   CardContent,
@@ -5,19 +7,21 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { useEffect } from "react";
+import { generateMap, generateMarker, generateKakaoScript } from "../util";
 
 interface GatheringProps {
   title: string;
   date: Date;
   location: string;
-  image?: string;
+  id: string;
 }
 
 export default function Gathering({
   title,
   date,
   location,
-  image,
+  id,
 }: GatheringProps) {
   const newDate = new Intl.DateTimeFormat("ko-KR", {
     year: "numeric",
@@ -27,15 +31,29 @@ export default function Gathering({
     minute: "numeric",
   }).format(date);
 
+  useEffect(() => {
+    const kakaoMapScript = generateKakaoScript();
+
+    const onLoadKakaoAPI = () => {
+      window.kakao.maps.load(() => {
+        const map = generateMap(id);
+        const marker = generateMarker();
+
+        marker.setMap(map);
+      });
+    };
+
+    kakaoMapScript.addEventListener("load", onLoadKakaoAPI);
+  }, [id]);
   return (
-    <Card className="hover:-translate-y-2 transition-all mx-auto p-1">
+    <Card className="hover:-translate-y-2 transition-all mx-auto p-1 pb-0">
       <CardHeader className="pb-2 mb-3">
         <CardTitle>{title}</CardTitle>
         <CardDescription>{newDate}</CardDescription>
       </CardHeader>
-      <CardContent className="align-middle">
-        {image || <div className="size-44 mx-auto bg-slate-600 rounded-md" />}
-        <p className="mt-3">{location}</p>
+      <CardContent className="align-middle -mb-2">
+        <div id={id} className="size-44 mx-auto bg-slate-600 rounded-md" />
+        <p className="mt-4 mb-1">{location}</p>
       </CardContent>
     </Card>
   );

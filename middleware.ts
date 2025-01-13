6292b05a4105515bc8e7bb7a4c3b0f5e92f07d7a
JeongHeon <kdrlawjdgjs@gmail.com>
2025-01-13 13:35:37 +0900

    refactor: 의미없는 코드 삭제
    - 쿠키에 새로운 값을 추가하는 방식은 다른 이벤트를 발생시키지 못함.
    - 쿠키 응답을 통해 이벤트를 발생시키기보다는 클릭 이벤트내에서 다루는 것이 더 직관적으로 보임
    - 그래서 session만 사용하도록 코드 다시 수정

--- middleware.ts
+++ middleware.ts
@@ -3,22 +3,21 @@ import { Auth } from "./app/api/auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const session = (await cookies()).get("session");
+  const cookieStorage = await cookies();
+  const session = cookieStorage.get("session");
   const response = NextResponse.next();
 
   if (request.url.endsWith("/") && session) {
     await Auth.updateSession();
-    response.cookies.set("isLogin", "true");
+
     return response;
   }
 
   if (request.url.includes("/newGathering") && !session) {
-    response.cookies.set("isLogin", "false");
     return NextResponse.rewrite(new URL("/", request.nextUrl));
   }
 
   if (request.url.includes("/myClimbing") && !session) {
-    response.cookies.set("isLogin", "false");
     return NextResponse.rewrite(new URL("/", request.nextUrl));
   }
 }

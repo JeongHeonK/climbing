7fdaedd72842e2295311ad7d4e96722efe1865a1
JeongHeon <kdrlawjdgjs@gmail.com>
2025-01-13 17:54:30 +0900

    feat: 함수 수정
    - 서버 액션으로 전환
    - 다음값 확인 로직 수정

--- app/(home)/actions/getGatherings.ts
+++ app/(home)/actions/getGatherings.ts
@@ -1,6 +1,8 @@
+"use server";
+
 import { connectDB } from "@/app/api/database";
 
-interface Gathering {
+export interface IGathering {
   _id: string;
   user: string;
   title: string;
@@ -14,14 +16,19 @@ export const getGathering = async (page = 1) => {
   const skipCount = (page - 1) * 8;
 
   const db = (await connectDB).db("climbing");
-  const result = db
-    .collection<Gathering>("gathering")
+  const result = await db
+    .collection<IGathering>("gathering")
     .find({})
     .skip(skipCount)
-    .limit(8)
-    .sort({ date: -1 });
+    .limit(9)
+    .sort({ date: -1 })
+    .toArray();
 
-  const gatherings = (await result.hasNext()) ? await result.toArray() : [];
+  const gatherings = result.slice(0, 8).map((data) => ({
+    ...data,
+    _id: String(data._id),
+  }));
 
-  return gatherings;
+  const hasNext = result.length > 8;
+  return { gatherings, hasNext };
 };

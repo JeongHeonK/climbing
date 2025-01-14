6bfb9e4c3689ceb17c171d858e013b4af84c0010
JeongHeon <kdrlawjdgjs@gmail.com>
2025-01-14 15:52:05 +0900

    feat: intercepting router modal wrapper

---
+++ app/(home)/components/ModalWrapper.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { PropsWithChildren } from "react";
+import { useRouter } from "next/navigation";
+
+export default function ModalWrapper({ children }: PropsWithChildren) {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.back();
+  };
+
+  return (
+    <div
+      className="bg-slate-900/50 fixed z-40 inset-0 text-white"
+      onClick={handleClick}
+    >
+      {children}
+    </div>
+  );
+}

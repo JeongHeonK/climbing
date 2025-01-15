38f27517b71799571beb18504fbcc1672fee697c
JeongHeon <kdrlawjdgjs@gmail.com>
2025-01-15 18:03:29 +0900

    refactor: 추상화를 통해 한 컴포넌트에서 수정과 생성 삭제 모두 통제
    - Id 존재 유무로 판단
    - 없으면 생성
    - 있으면 수정 혹은 삭제

---
+++ app/(gathering)/components/GatheringPageScene.tsx
@@ -0,0 +1,17 @@
+import { getGathering } from "@/app/(home)/actions/getGatherings";
+import MeetingForm from "./MeetingForm";
+
+export default async function GatheringPageScene({ id }: { id?: string }) {
+  const gathering = id ? await getGathering(id) : null;
+
+  return (
+    <MeetingForm
+      _id={gathering?._id.toString()}
+      title={gathering?.title}
+      description={gathering?.description}
+      lat={gathering?.lat}
+      lng={gathering?.lng}
+      date={gathering?.date}
+    />
+  );
+}

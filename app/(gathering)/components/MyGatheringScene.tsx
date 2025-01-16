3236c8309891dee9d27b71337a2e184db7818e48
JeongHeon <kdrlawjdgjs@gmail.com>
2025-01-16 23:50:27 +0900

    feat: 찜하기 구현 완료

--- app/(gathering)/components/MyGatheringScene.tsx
+++ app/(gathering)/components/MyGatheringScene.tsx
@@ -4,6 +4,7 @@ import DefaultGathering from "@/app/(home)/components/DefaultGathering";
 import { MyGathering } from "@/app/(home)/types/type";
 import { useEffect, useState } from "react";
 import MyGatherings from "./MyGatherings";
+import { getMyGathering } from "../actions/gatheringActions";
 
 export interface LikeButtonData {
   id: string;
@@ -15,19 +16,21 @@ export default function MyGatheringScene({ isLogin }: { isLogin: boolean }) {
 
   useEffect(() => {
     const result = window.localStorage.getItem("mine");
-
-    if (!result) {
-      return;
-    }
-    const newMyGathering: LikeButtonData[] = JSON.parse(result);
-    const ids = newMyGathering.map((gathering) => gathering.id);
-    console.log(ids);
-    setMyGatherings([]);
+    const updateMyGathering = async () => {
+      if (!result) {
+        return;
+      }
+      const myGatheringData: LikeButtonData[] = JSON.parse(result);
+      const ids = myGatheringData.map((gathering) => gathering.id);
+      const myGatherings = await getMyGathering(ids);
+      setMyGatherings(myGatherings);
+    };
+    updateMyGathering();
   }, []);
 
   return (
     <div className="max-w-[1100px] mx-auto">
-      <h3 className="ml-7 p-1 font-semibold">Join us</h3>
+      <h3 className="ml-7 p-1 font-semibold">My Climbing </h3>
       {myGatherings.length === 0 ? (
         <DefaultGathering kind="mine" />
       ) : (

1033102d4ad36426f1fba5482cea1461720e91f2
JeongHeon <kdrlawjdgjs@gmail.com>
2025-01-20 18:38:58 +0900

    feat: 삭제 테스트 완료

---
+++ cypress/e2e/deleteGathering.cy.ts
@@ -0,0 +1,43 @@
+describe("delete gathering fail", () => {
+  before(() => {
+    const email = Cypress.env("testId2");
+    const pw = Cypress.env("testPw2");
+    const baseUrl = Cypress.env("baseUrl");
+
+    cy.visit(baseUrl);
+    cy.contains("Log In").click();
+    cy.get("#email").type(email);
+    cy.get("#password").type(pw);
+    cy.get(".grid > .inline-flex").click();
+    cy.wait(100);
+  });
+
+  it("should not show edit btn when other user", () => {
+    cy.contains("cypress 수정 테스트").should("exist");
+    cy.contains("cypress 수정 테스트").click();
+    cy.get('[data-cy="editPage"]').should("not.exist");
+  });
+});
+
+describe("delete gathering fail", () => {
+  before(() => {
+    const email = Cypress.env("testId");
+    const pw = Cypress.env("testPw");
+    const baseUrl = Cypress.env("baseUrl");
+
+    cy.visit(baseUrl);
+    cy.contains("Log In").click();
+    cy.get("#email").type(email);
+    cy.get("#password").type(pw);
+    cy.get(".grid > .inline-flex").click();
+    cy.wait(100);
+  });
+
+  it("should not show edit btn when other user", () => {
+    cy.contains("cypress 수정 테스트").should("exist");
+    cy.contains("cypress 수정 테스트").click();
+    cy.get('[data-cy="editPage"]').click();
+    cy.contains("삭제").click();
+    cy.contains("cypress 수정 테스트").should("not.exist");
+  });
+});

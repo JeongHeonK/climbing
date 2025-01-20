c3ed87015fa51e5f4c8fd7dc27a658b7d129d6b0
JeongHeon <kdrlawjdgjs@gmail.com>
2025-01-20 15:21:00 +0900

    feat: 로그인 전 테스트 완료

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,7 +1,8 @@
-describe("Home page", () => {
+describe("Home page before login", () => {
   it("should show header without login", () => {
     cy.visit("/");
     cy.get("a").first().should("have.text", "HOME");
+    cy.contains("Join us").should("exist");
     cy.get("a").eq(1).should("have.text", "MY CLIMBING");
     cy.get("button").first().should("have.text", "Log In");
   });
@@ -24,6 +25,20 @@ describe("Home page", () => {
     cy.visit("/");
     cy.getCookie("session").should("not.exist");
     cy.get(".px-auto > :nth-child(2)").click("bottom");
+    cy.contains("로그인 후 이용해주세요").as("alert");
+    cy.get("@alert").should("exist");
+    cy.get(".group > .absolute").click();
+    cy.get("@alert").should("not.exist");
+    cy.get("[data-cy='modal-bg']").as("modal-bg");
+    cy.get("@modal-bg").should("exist");
+    cy.get("@modal-bg").click("bottom");
+    cy.get("@modal-bg").should("not.exist");
+  });
+
+  it("can't make new gathering", () => {
+    cy.visit("/");
+    cy.getCookie("session").should("not.exist");
+    cy.get("[data-cy='newGathering']").click();
     cy.contains("로그인 후 이용해주세요").as("alert");
     cy.get("@alert").should("exist");
     cy.get(".group > .absolute").click();

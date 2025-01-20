3774f0575c18b596af5ac1df3ff63063c4066fa0
JeongHeon <kdrlawjdgjs@gmail.com>
2025-01-20 15:13:25 +0900

    feat: test 분리
    - 디테일과 마이클라이밍 접근 따로 테스트

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -6,10 +6,24 @@ describe("Home page", () => {
     cy.get("button").first().should("have.text", "Log In");
   });
 
-  it("can't access detail or my page without login", () => {
+  it("can't access myClimbing page without login", () => {
     cy.visit("/");
     cy.getCookie("session").should("not.exist");
     cy.get("a").eq(1).click();
+    cy.contains("로그인 후 이용해주세요").as("alert");
+    cy.get("@alert").should("exist");
+    cy.get(".group > .absolute").click();
+    cy.get("@alert").should("not.exist");
+    cy.get("[data-cy='modal-bg']").as("modal-bg");
+    cy.get("@modal-bg").should("exist");
+    cy.get("@modal-bg").click("bottom");
+    cy.get("@modal-bg").should("not.exist");
+  });
+
+  it("can't access detail without login", () => {
+    cy.visit("/");
+    cy.getCookie("session").should("not.exist");
+    cy.get(".px-auto > :nth-child(2)").click("bottom");
     cy.contains("로그인 후 이용해주세요").as("alert");
     cy.get("@alert").should("exist");
     cy.get(".group > .absolute").click();

3b0547db8e9e87259fdc6f4e9c295eceaadff06d
JeongHeon <kdrlawjdgjs@gmail.com>
2025-01-22 16:45:32 +0900

    fix: undefined일 경우 실행 중지하도록 변환

--- app/(gathering)/editGathering/[id]/page.tsx
+++ app/(gathering)/editGathering/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import GatheringPageScene from "../../components/GatheringPageScene";
 
 export default async function EditGatheringPage({
@@ -6,5 +7,8 @@ export default async function EditGatheringPage({
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
+
+  if (id === undefined) notFound();
+
   return <GatheringPageScene id={id} />;
 }

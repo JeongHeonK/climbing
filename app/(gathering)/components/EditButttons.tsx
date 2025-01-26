06754cced1b40de0ff35b42a3df7f2fe0b239598
JeongHeon <kdrlawjdgjs@gmail.com>
2025-01-26 17:30:14 +0900

    feat: 삭제 버튼 유저 피드백 추가

--- app/(gathering)/components/EditButttons.tsx
+++ app/(gathering)/components/EditButttons.tsx
@@ -1,23 +1,35 @@
 import { useFormStatus } from "react-dom";
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
+import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import Spinner from "@/app/components/common/Spinner";
 import { deleteGathering } from "../actions/gatheringActions";
 
 export default function EditButtons({ id }: { id: string }) {
-  const { pending } = useFormStatus();
-
-  const handleDelete = async (e: MouseEvent) => {
-    e.preventDefault();
-    await deleteGathering(id);
-  };
+  const { pending, handleDelete, isLoading } = useEditButton(id);
 
   return (
     <span className="flex gap-4">
       <Button>{pending ? <Spinner /> : "수정"}</Button>
-      <Button className="bg-red-500" type="button" onClick={handleDelete}>
-        삭제
+      <Button className="bg-red-500 " type="button" onClick={handleDelete}>
+        {isLoading ? <Spinner /> : "삭제"}
       </Button>
     </span>
   );
 }
+
+const useEditButton = (id: string) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { pending } = useFormStatus();
+  const router = useRouter();
+
+  const handleDelete = async (e: MouseEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+    await deleteGathering(id);
+    setIsLoading(false);
+    router.push("/");
+  };
+
+  return { isLoading, pending, handleDelete };
+};

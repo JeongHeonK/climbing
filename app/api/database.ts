6513c2c90429b8152e049bcd49715548976daa6e
JeongHeon <kdrlawjdgjs@gmail.com>
2025-01-30 19:17:24 +0900

    feat: env 수정

--- app/api/database.ts
+++ app/api/database.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGO_DB_URL!;
+const uri = process.env.NEXT_PUBLIC_MONGO_DB_URL!;
 const db = new MongoClient(uri).db("climbing");
 
 export { db };

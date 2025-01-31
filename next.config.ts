72819feace12bebb2f09692a4aab9795577bd11b
JeongHeon <kdrlawjdgjs@gmail.com>
2025-01-31 15:38:53 +0900

    chore: 설정 변경

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    MONGO_DB_URL: process.env.MONGO_DB_URL,
+    SESSION_SECRET: process.env.SESSION_SECRET,
+    NEXT_PUBLIC_KAKAO_KEY: process.env.NEXT_PUBLIC_KAKAO_KEY,
+  },
 };
 
 export default nextConfig;
